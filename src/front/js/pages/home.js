b60919ae5a3007b777fd4b5afd8525bd0f626cb6
Pedro Parra <91670052+PedroParraPerez@users.noreply.github.com>
2022-03-14 17:15:07 +0100

    Update home.js

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -12,6 +12,7 @@ export const Home = () => {
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
+<h1>HOLAAAAAA</h1>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
